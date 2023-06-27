3f1504970d96a47b38d1a6aef2ede01baaf95268
Nishantha Ukkubandage <575774@ACDM.DS.SAIT.CA>
2023-06-27 13:57:05 -0600

    DBchanges

--- database.sql
+++ database.sql
@@ -263,3 +263,30 @@ ALTER TABLE [MortgageRates]  WITH CHECK ADD  CONSTRAINT [FK_MortgageRates_Mortga
 REFERENCES [MortgageRateTypes] ([RateTypeID])
 GO
 
+/****** Object:  Table [dbo].[InflationvsPrices]    Script Date: 6/27/2023 12:55:12 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[InflationvsPrices](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Date] [int] NULL,
+	[Price] [float] NULL,
+	[Rate] [float] NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_InflationvsPrices_ID] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[InflationvsPrices] ADD  CONSTRAINT [DF_InflationvsPrices_rowguid]  DEFAULT (newid()) FOR [rowguid]
+GO
+
+ALTER TABLE [dbo].[InflationvsPrices] ADD  CONSTRAINT [DF_InflationvsPrices_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+
