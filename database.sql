ac2e63b8625b826a29de7db00e742b9f6c577a5f
Nishantha Ukkubandage <575774@ACDM.DS.SAIT.CA>
2023-06-28 11:55:03 -0600

    db chnages in progress

--- database.sql
+++ database.sql
@@ -30,74 +30,7 @@ ALTER TABLE [Province] ADD  CONSTRAINT [DF_Province_ModifiedDate]  DEFAULT (getd
 GO
 
 
-/****** Object:  Table [HouseType]     ******/
 
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [HouseType](
-	[TypeID] [int] IDENTITY(1,1) NOT NULL,
-	[Name] [varchar](50) NOT NULL,
-	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
-	[ModifiedDate] [datetime] NOT NULL,
- CONSTRAINT [PK_HouseType_TypeID] PRIMARY KEY CLUSTERED
- (
- [TypeID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [HouseType] ADD  CONSTRAINT [DF_Name] UNIQUE(Name)
-
-ALTER TABLE [HouseType] ADD  CONSTRAINT [DF_HouseType_rowguid]  DEFAULT (newid()) FOR [rowguid]
-GO
-
-ALTER TABLE [HouseType] ADD  CONSTRAINT [DF_HouseType_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
-GO
-
-/****** Object:  Table [HousePrices]     ******/
-
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [HousePrices](
-	[HousePriceID] [int] IDENTITY(1,1) NOT NULL,
-	[Date] [DATE],
-	[HouseType] [int] NOT NULL,
-	[Province] [int] NOT NULL,
-	[BenchMarkPrice] [money] NOT NULL,
-	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
-	[ModifiedDate] [datetime] NOT NULL,
- CONSTRAINT [PK_HousePrices_HousePriceID] PRIMARY KEY CLUSTERED
- (
- [HousePriceID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-
-
-ALTER TABLE [HousePrices] ADD  CONSTRAINT [DF_HousePrices_rowguid]  DEFAULT (newid()) FOR [rowguid]
-GO
-
-ALTER TABLE [HousePrices] ADD  CONSTRAINT [DF_HousePrices_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
-GO
-
-ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_HouseType_TypeID] FOREIGN KEY([HouseType])
-REFERENCES [HouseType] ([TypeID])
-GO
-
-ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_Province_ProvinceID] FOREIGN KEY([Province])
-REFERENCES [Province] ([ProvinceID])
-GO
 
 
 /****** Object:  Table [CanadaGDP]     ******/
@@ -263,30 +196,82 @@ ALTER TABLE [MortgageRates]  WITH CHECK ADD  CONSTRAINT [FK_MortgageRates_Mortga
 REFERENCES [MortgageRateTypes] ([RateTypeID])
 GO
 
-/****** Object:  Table [dbo].[InflationvsPrices]    Script Date: 6/27/2023 12:55:12 PM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[InflationvsPrices](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Date] [int] NULL,
-	[Price] [float] NULL,
-	[Rate] [float] NOT NULL,
+CREATE TABLE [HouseType](
+	[TypeID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
 	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
 	[ModifiedDate] [datetime] NOT NULL,
- CONSTRAINT [PK_InflationvsPrices_ID] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC
+ CONSTRAINT [PK_HouseType_TypeID] PRIMARY KEY CLUSTERED
+ (
+ [TypeID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[InflationvsPrices] ADD  CONSTRAINT [DF_InflationvsPrices_rowguid]  DEFAULT (newid()) FOR [rowguid]
+ALTER TABLE [HouseType] ADD  CONSTRAINT [DF_Name] UNIQUE(Name)
+
+ALTER TABLE [HouseType] ADD  CONSTRAINT [DF_HouseType_rowguid]  DEFAULT (newid()) FOR [rowguid]
 GO
 
-ALTER TABLE [dbo].[InflationvsPrices] ADD  CONSTRAINT [DF_InflationvsPrices_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
+ALTER TABLE [HouseType] ADD  CONSTRAINT [DF_HouseType_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
 GO
 
+/****** Object:  Table [HousePrices]     ******/
+
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [HousePrices](
+	[HousePriceID] [int] IDENTITY(1,1) NOT NULL,
+	[Date] [DATE],
+	[HouseType] [int] NOT NULL,
+	[MortgageRates] [int],
+	Inflation [int],
+	[Province] [int] NOT NULL,
+	[BenchMarkPrice] [money] NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_HousePrices_HousePriceID] PRIMARY KEY CLUSTERED
+ (
+ [HousePriceID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+ALTER TABLE [HousePrices] ADD  CONSTRAINT [DF_HousePrices_rowguid]  DEFAULT (newid()) FOR [rowguid]
+GO
+
+ALTER TABLE [HousePrices] ADD  CONSTRAINT [DF_HousePrices_ModifiedDate]  DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+
+ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_HouseType_TypeID] FOREIGN KEY([HouseType])
+REFERENCES [HouseType] ([TypeID])
+GO
+
+ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_Province_ProvinceID] FOREIGN KEY([Province])
+REFERENCES [Province] ([ProvinceID])
+GO
+
+ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_Inflation_ProvinceID] FOREIGN KEY([Inflation])
+REFERENCES [Inflation] ([InflationID])
+GO
+
+ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_Inflation_ProvinceID] FOREIGN KEY([Inflation])
+REFERENCES [Inflation] ([InflationID])
+GO
+
+ALTER TABLE [HousePrices]  WITH CHECK ADD  CONSTRAINT [FK_HousePrices_MortgageRates_MortgageRateID] FOREIGN KEY([MortgageRates])
+REFERENCES [MortgageRates] ([MortgageRateID])
+GO
